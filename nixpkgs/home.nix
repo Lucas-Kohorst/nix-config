14f15be8057f534fb8c75d68723296a8acdfd8d2
Lucas Kohorst <kohorstlucas@gmail.com>
2022-07-21 13:55:45 -0400

    Update home.nix

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -4,6 +4,14 @@
   imports = [
     <home-manager/nixos>
   ];
+  
+  home = {
+    username = "lucas";
+    homeDirectory = "/home/lucas";
+    stateVersion = "22.05";
+  };
+
+  programs.home-manager.enable = true;
 
   home-manager.users.lucas = { pkgs, ... }: {
     imports = [
